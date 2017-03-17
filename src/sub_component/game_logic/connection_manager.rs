572924ad66454a670bce851d0f01c9eeaf820a80
myoshida <myoshida@aiming-inc.com>
2017-03-17 14:14:38 +0900

    prog

--- src/sub_component/game_logic/connection_manager.rs
+++ src/sub_component/game_logic/connection_manager.rs
@@ -1,3 +1,5 @@
+extern crate byteorder;
+
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::collections::{HashMap,VecDeque};
@@ -6,7 +8,8 @@ use std::sync::Arc;
 use std::thread;
 use std::net::ToSocketAddrs;
 use std::net::SocketAddr;
-use std::io::{Write,Read};
+use std::io::{Write,Read,BufReader,BufRead};
+use self::byteorder::{BigEndian,ByteOrder};
 use entity_component::component::*;
 
 type ConnectionID = u32;
@@ -19,10 +22,65 @@ enum EventStoC {
     SendTo((TcpStream,Vec<u8>)),
 }
 
+struct DataHeader {
+    body_len:i32,
+}
+
+impl DataHeader {
+    fn new() -> DataHeader {
+        DataHeader {
+            body_len:0,
+        }
+    }
+}
+
+struct Connection {
+    send_stream:TcpStream,
+    recv_buffer:BufReader<TcpStream>,
+    recv_header:DataHeader,
+}
+
+impl Connection {
+    fn new(stream:TcpStream) -> Connection {
+        let buf = BufReader::with_capacity(1024, stream.try_clone().unwrap());
+        Connection {
+            send_stream:stream,
+            recv_buffer:buf,
+            recv_header:DataHeader::new(),
+        }
+    }
+    fn recv(&mut self) {
+
+
+        let mut raw:[u8;4] = [0;4];
+        let mut consumed = false;
+        match self.recv_buffer.fill_buf() {
+            Err(e) => println!("error {} {} {}", file!(), line!(), e),
+            Ok(data) => {
+                println!("data len {}", data.len());
+                if data.len() >= 4 {
+                    for i in 0..4 {
+                        raw[i] = *data.get(i).unwrap();
+                    }
+                    consumed = true;
+                }
+            }
+        }
+        if consumed {
+            self.recv_header.body_len = BigEndian::read_i32(&raw[..]);
+            self.recv_buffer.consume(4);
+            println!("read {}", self.recv_header.body_len);
+        }
+    }
+    fn send(&mut self, data:&[u8]) {
+        self.send_stream.write_all(data);
+    }
+}
+
 pub struct ConnectionManager {
     next_conn_id:ConnectionID,
     listener:TcpListener,
-    connections:HashMap<ConnectionID,TcpStream>,
+    connections:HashMap<ConnectionID, Connection>,
 }
 
 impl ConnectionManager {
@@ -36,8 +94,8 @@ impl ConnectionManager {
         }
     }
     pub fn send_to(&mut self, conn_id:ConnectionID, data:Vec<u8>) {
-        if let Some(conn) = self.connections.get_mut(&conn_id) {
-            conn.write_all(data.as_slice());
+        if let Some(val) = self.connections.get_mut(&conn_id) {
+            val.send(data.as_slice());
         }
     }
 }
@@ -50,7 +108,7 @@ impl SubComponent for ConnectionManager {
         match self.listener.accept() {
             Ok((stream, addr)) => {
                 stream.set_nonblocking(true);
-                self.connections.insert(self.next_conn_id, stream);
+                self.connections.insert(self.next_conn_id, Connection::new(stream));
                 println!("new connection {} {}", self.next_conn_id, addr);
                 self.next_conn_id += 1;
             },
@@ -59,18 +117,15 @@ impl SubComponent for ConnectionManager {
             }
         }
 
-        for (conn_id, stream) in self.connections.iter_mut() {
+        for (conn_id, conn) in self.connections.iter_mut() {
 
-            if let Ok(Some(e)) = stream.take_error() {
-                println!("error on socket {}", e);
-            }
+            // let ref mut stream = str_buf.0;
+            // let ref mut buffer = str_buf.1;
+            // if let Ok(Some(e)) = stream.take_error() {
+            //     println!("error on socket {}", e);
+            // }
+            conn.recv();
 
-            let mut header:[u8;1] = [0;1];
-            match stream.read_exact(&mut header) {
-                Err(e) => println!("error {} {} {}", file!(), line!(), e),
-                _ => {}
-            }
-            println!("read {:?}", header);
         }
 
         // for each connection recv data
