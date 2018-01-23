b8572b6b3fdf9913f00e8abd9cf696b5064bb176
myoshid <myoshida@aiming-inc.co>
2018-01-23 21:07:15 +0900

    websocket

--- ragtime_sample1/src/main.rs
+++ ragtime_sample1/src/main.rs
@@ -1,85 +1,72 @@
 #[macro_use]
 extern crate ragtime;
+extern crate ws;
 
-mod components;
-mod receptor;
-mod game_scene;
-mod protocol;
+struct Router {
+    // sender: ws::Sender,
+    // inner: Box<ws::Handler>,
+}
 
-use std::cell::RefCell;
-use std::rc::{Weak, Rc};
-use std::time::Duration;
-use std::thread;
-use std::collections::HashMap;
-use std::sync::mpsc::{channel,Receiver,Sender};
-use receptor::*;
+impl ws::Handler for Router {
+    fn on_request(&mut self, req: &ws::Request) -> ws::Result<(ws::Response)> {
 
-//use title_scene::room::*;
-use game_scene::room::*;
-use protocol::*;
+        println!("====== {}", req.resource());
+        let mut res = try!(ws::Response::from_request(req));
+        res.set_status(404);
+        res.set_reason("Not Found");
+        Ok(res)
 
-use ragtime::network::*;
-use ragtime::db::*;
-use ragtime::room::*;
+    }
 
-type UserID = u64;
+    // Pass through any other methods that should be delegated to the child.
+    //
+    // You could probably use a macro for this if you have many different
+    // routers or were building some sort of routing framework.
+
+    fn on_shutdown(&mut self) {
+    }
+
+    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
+        Ok(())
+    }
+
+    fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: ws::CloseCode, reason: &str) {
+    }
+
+    fn on_error(&mut self, err: ws::Error) {
+    }
+}
 
 struct Sample1Game {
-//    title_room_mgr: RoomManager<TitleRoom>,
-    game_room_mgr:RoomManager<Sample1Room>,
-    conn_mgr: ConnectionManager<Command>,
-    db_manager: DBManager,
-    user_mgr: Vec<(UserID,ConnectionID)>,
+//    game_room_mgr:RoomManager<Sample1Room>,
 }
 
 impl Sample1Game {
     fn new() -> Sample1Game {
-
-        let mut conn = ConnectionManager::<_>::new("127.0.0.1:8080".to_string());
-
-        let grm = RoomManager::<Sample1Room>::new(2);
-        let db = DBManager::new();
-
-        Sample1Game {
-//             title_room_mgr:
-            game_room_mgr: grm,
-            conn_mgr: conn,
-            db_manager: db,
-        }
+        Sample1Game{}
     }
     fn update(&mut self) {
-        if let Some(conn_id) = self.conn_mgr.listen() {
-            
-        }
-        self.conn_mgr.recv(|conn_id,msg| {
-            match msg {
-                // if C2S command, resulve UserID and send to room
-                
-                Command::Login(c) => {
-                    self.user_mgr.push( (conn_id, c.user_id) );
-                },
-                Command::Join(c) => {
-                    // find conn id and get user_id
-                    // create_room
-                    // send cmd
-                }
-                _ => {},
-            }
-        });
-        
-//         if let Some(cxn) = self.conn_mgr.poll() {
-//             let room_id = self.game_room_mgr.create_room();
-//             let cmd = JoinCommand::new(1,cxn.1,cxn.2);
-//             self.game_room_mgr.send_cmd(room_id, RoomCommand::Join(cmd));
-//         }
+    }
+    fn on_connect(&self, sender: ws::Sender) -> Router {
+        Router{}
     }
 }
 
 pub fn sample1_start() {
     let mut game = Sample1Game::new();
-    loop {
-        game.update();
-        thread::sleep(Duration::from_millis(1000));
+    // loop {
+    //     game.update();
+    //     thread::sleep(Duration::from_millis(1000));
+    // }
+   if let Err(error) = ws::listen("127.0.0.1:3012", |out| {
+       game.on_connect(out)
+    })
+     {
+        println!("Failed to create WebSocket due to {:?}", error);
     }
 }
 
